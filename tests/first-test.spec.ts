00edc3ab4149ce5d2dfd4cc0896ebaace4f68e7f
Sam Greenwood <sam_greenwood26@hotmail.com>
2026-08-09 11:59:25 +0100

    Refactor tests to use beforeEach for setup and improve readability of assertions for Dyson manufacturer page

--- tests/first-test.spec.ts
+++ tests/first-test.spec.ts
@@ -2,53 +2,48 @@
 // directly) so tests get access to the custom Page Object fixtures.
 import { test, expect } from '../fixtures/test-options';
 
-// Test 01 - Navigate to the Dyson manufacturer page and assert the URL is correct.
-test('navigate to the Dyson homepage', async ({ page }) => {
- await page.goto('https://source.thenbs.com/en/gb');
-  await page.getByRole('textbox', { name: 'Search' }).click();
-  await page.getByRole('textbox', { name: 'Search' }).fill('Dyson');
-  await page.getByRole('textbox', { name: 'Search' }).press('Enter');
-  await page.getByRole('tab', { name: 'Manufacturers' }).click();
-  await page.getByRole('link', { name: 'Dyson Dyson Technology for' }).click();
+test.beforeEach(async ({ page }) => {
+  // Navigate to the Dyson Manufacturer homepage before each test.
+  const searchInput = page.getByRole('textbox', { name: 'Search' });
+  const manufacturerTab = page.getByRole('tab', { name: 'Manufacturers' });
+  const dysonLink = page.getByRole('link', { name: 'Dyson Dyson Technology for' });
+
+  await page.goto('https://source.thenbs.com/en/gb');
+  await searchInput.click();
+  await searchInput.fill('Dyson');
+  await searchInput.press('Enter');
+  await manufacturerTab.click();
+  await dysonLink.click();
   await expect(page).toHaveURL('https://source.thenbs.com/en/gb/manufacturer/dyson/nakAxHWxDZprdqkBaCdn4U/overview');
 });
 
-// Test 02 - Navigate to the Dyson manufacturer page and assert the h1 heading is correct.
+// Test 01 - Navigate to the Dyson manufacturer page and assert the h1 heading is correct.
 test('assert the h1 heading is correct', async ({ page }) => {
- await page.goto('https://source.thenbs.com/en/gb');
-  await page.getByRole('textbox', { name: 'Search' }).click();
-  await page.getByRole('textbox', { name: 'Search' }).fill('Dyson');
-  await page.getByRole('textbox', { name: 'Search' }).press('Enter');
-  await page.getByRole('tab', { name: 'Manufacturers' }).click();
-  await page.getByRole('link', { name: 'Dyson Dyson Technology for' }).click();
-  await expect(page.locator('h1')).toContainText('Dyson');
-  await expect(page.getByText('Technology for business', { exact: true })).toBeVisible();
+  const h1Heading = page.locator('h1');
+  const h1HeadingText = page.getByText('Technology for business', { exact: true });
+
+  await expect(h1Heading).toBeVisible();
+  await expect(h1Heading).toContainText('Dyson');
+  await expect(h1HeadingText).toHaveText('Technology for business');
+  await expect(h1HeadingText).toBeVisible();
 });
 
-// Test 03 - Navigate to the Dyson manufacturer page and assert the telephone number is correct.
+// Test 02 - Navigate to the Dyson manufacturer page and assert the telephone number is correct.
 test('assert the telephone number is correct', async ({ page }) => {
- await page.goto('https://source.thenbs.com/en/gb');
-  await page.getByRole('textbox', { name: 'Search' }).click();
-  await page.getByRole('textbox', { name: 'Search' }).fill('Dyson');
-  await page.getByRole('textbox', { name: 'Search' }).press('Enter');
-  await page.getByRole('tab', { name: 'Manufacturers' }).click();
-  await page.getByRole('link', { name: 'Dyson Dyson Technology for' }).click();
-  await expect(page.getByRole('link', { name: '08003457788' })).toBeVisible();
-  await expect(page.getByRole('link', { name: '08003457788' })).toContainText('08003457788');
-  await expect(page.getByRole('link', { name: '08003457788' })).toHaveAttribute('href', 'tel:08003457788');
+  const telephoneNumber = page.getByRole('link', { name: '08003457788' });
+
+  await expect(telephoneNumber).toBeVisible();
+  await expect(telephoneNumber).toContainText('08003457788');
+  await expect(telephoneNumber).toHaveAttribute('href', 'tel:08003457788');
 });
 
-// Test 04 - Navigate to the Dyson manufacturer page and assert the website link is correct.
+// Test 03 - Navigate to the Dyson manufacturer page and assert the website link is correct.
 test('assert the website link is correct', async ({ page }) => {
- await page.goto('https://source.thenbs.com/en/gb');
-  await page.getByRole('textbox', { name: 'Search' }).click();
-  await page.getByRole('textbox', { name: 'Search' }).fill('Dyson');
-  await page.getByRole('textbox', { name: 'Search' }).press('Enter');
-  await page.getByRole('tab', { name: 'Manufacturers' }).click();
-  await page.getByRole('link', { name: 'Dyson Dyson Technology for' }).click();
-  await expect(page.getByRole('link', { name: 'Website' })).toBeVisible();
-  await expect(page.getByRole('link', { name: 'Website' })).toContainText('Website');
-  await expect(page.getByRole('link', { name: 'Website' })).toHaveAttribute('href', 'https://www.dyson.co.uk/commercial/overview');
-  await expect(page.getByRole('link', { name: 'Website' })).toHaveAttribute('target', '_blank');
+  const websiteLink = page.getByRole('link', { name: 'Website' });
+
+  await expect(websiteLink).toBeVisible();
+  await expect(websiteLink).toContainText('Website');
+  await expect(websiteLink).toHaveAttribute('href', 'https://www.dyson.co.uk/commercial/overview');
+  await expect(websiteLink).toHaveAttribute('target', '_blank');
 });
 
